133519ef514c053513fd85d7f304e3e43140ff48
chuan tian <chtian@icsubmit01.sdcc.bnl.gov>
2017-06-07 10:45:56 -0400

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for psize in 2000;do
-for ngen in 50000 100000;do
+for ngen in 5000;do
 cat > parmfile.$psize.$ngen << EOF
 pSize=$psize;
 nGen=$ngen;
@@ -8,8 +8,8 @@ pMut=0.01
 max=0.5
 pCross=0.8
 rseed=314568
-peng=5000
-ncp=5000
+peng=$ngen
+ncp=$ngen
 EOF
 sbatch << EOF
 #!/bin/bash
@@ -21,10 +21,8 @@ sbatch << EOF
 #SBATCH --gres=gpu:1
 #SBATCH -t 30:00
 #SBATCH -o logfile.psize${psize}.ngen${ngen}.1gpu
-
-nvprof -o output.$psize.$ngen.nvp ./genA -p parmfile.${psize}.${ngen} -s scores.${psize}.${ngen}.dat < input > try.${psize}.${ngen}.frcmod
+./genA -p parmfile.${psize}.${ngen} -s scores.${psize}.${ngen}.dat < input > try.${psize}.${ngen}.frcmod
 rm parmfile.$psize.$ngen
-rm scores.${psize}.${ngen}.dat
 EOF
 done
 done
